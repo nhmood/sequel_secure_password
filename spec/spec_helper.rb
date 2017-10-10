abd0e8cd2be5bf47a319bfb512b1a32485043732
nhmood <nhmood@goosecode.com>
2017-10-10 18:08:42 -0400

    Update tests to handle Sequel 5 Schema plugin removal
    Sequel 5 no longer has the Schema plugin (Sequel::Model.plugin(:schema))
    as well as the #set_schema model methods
    Updated the spec_helper to initialize the SQLite inmemory db, define the
    schema, apply migrations, modify the models, then run the tests
    
    Reference for this test approach was taken from Padrino Admin sequel
    text fixture
    https://github.com/padrino/padrino-framework/blob/master/padrino-admin/test/fixtures/sequel.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,60 +3,72 @@ require 'bundler'
 Bundler.setup
 
 require 'sequel'
+require 'sequel/extensions/migration'
 require 'sequel/plugins/secure_password'
 
-adapter = RUBY_PLATFORM == "java" ? 'jdbc:sqlite::memory:' : 'sqlite:/'
 
 RSpec.configure do |c|
   c.before :suite do
-    Sequel::Model.plugin(:schema)
-    Sequel.connect adapter
 
+    # Based on the Ruby Engine, connect to the specified in memory SQLite db
+    Sequel::Model.db = begin
+      if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+        require 'jdbc/sqlite3'
+        Sequel.connect("jdbc:sqlite::memory:")
+      else
+        require 'sqlite3'
+        Sequel.sqlite(":memory:")
+      end # if defined?
+    end # Sequel::Model.db =
+
+
+    # Create the Sequel migration to populate the necessary user tables
+    migration = Sequel.migration do
+      up do
+        create_table(:users) do
+          primary_key :id
+          varchar     :password_digest
+        end # create_table(:users)
+
+        create_table(:high_cost_users) do
+          primary_key :id
+          varchar     :password_digest
+        end # create_table(:high_cost_users)
+
+        create_table(:user_without_validations) do
+          primary_key :id
+          varchar     :password_digest
+        end # create_table(:high_cost_users)
+
+        create_table(:user_with_alternate_digest_columns) do
+          primary_key :id
+          varchar     :password_hash
+        end # create_table(:high_cost_users)
+      end # up do
+    end # Sequel.migration
+
+    # Apply the migration against the SQLite in memory db we established earlier
+    migration.apply(Sequel::Model.db, :up)
+
+    # Configure the Sequel Models with various secure_password options
     class User < Sequel::Model
-      set_schema do
-        primary_key :id
-        varchar     :password_digest
-      end
-
       plugin :secure_password
     end
 
     class HighCostUser < Sequel::Model
-      set_schema do
-        primary_key :id
-        varchar     :password_digest
-      end
-
       plugin :secure_password, cost: 12
     end
 
     class UserWithoutValidations < Sequel::Model
-      set_schema do
-        primary_key :id
-        varchar     :password_digest
-      end
-
       plugin :secure_password, include_validations: false
     end
 
     class UserWithAlternateDigestColumn < Sequel::Model
-      set_schema do
-        primary_key :id
-        varchar     :password_hash
-      end
-
       plugin :secure_password, digest_column: :password_hash
     end
-
-    User.create_table!
-    HighCostUser.create_table!
-    UserWithoutValidations.create_table!
-    UserWithAlternateDigestColumn.create_table!
   end
 
   c.around :each do |example|
     Sequel::Model.db.transaction(:rollback => :always) { example.run }
   end
 end
-
-
